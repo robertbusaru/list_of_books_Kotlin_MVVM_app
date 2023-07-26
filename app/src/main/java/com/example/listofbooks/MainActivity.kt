4041e30899600953c8681e1e73ba0b951d25d5df
Robert Busaru <rbusaru@gmail.com>
2023-07-26 12:09:04 +0300

    imports optimized -> main activity

--- app/src/main/java/com/example/listofbooks/MainActivity.kt
+++ app/src/main/java/com/example/listofbooks/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.listofbooks
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
